c01d561e99b0195465225383a17ec90c61d2c0e6
Fanis Kalimullin <fanis@innosoft.pro>
2019-10-19 11:53:07 +0300

    Initial commit

---
+++ product_a/test.py
@@ -0,0 +1,14 @@
+import random
+import unittest
+from time import sleep
+from common import sum
+
+
+class TestCase(unittest.TestCase):
+    def test_very_long(self):
+        sleep(60 * 10)
+        self.assertTrue(sum(1, 1) == 2)
+
+    def test_random_fail(self):
+        if random.random() < 0.1:
+            self.assertTrue(False)
